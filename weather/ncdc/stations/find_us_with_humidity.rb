8a4da69780fdaecfb92e28d20efb92b58549c19b
Philip (flip) Kromer <flip@infochimps.org>
2008-07-31 21:39:16 -0500

    OK they spell humidity as 'dew point'.  dammit, now to go fix this.

--- weather/ncdc/stations/find_us_with_humidity.rb
+++ weather/ncdc/stations/find_us_with_humidity.rb
@@ -3,7 +3,7 @@ require 'imw/utils'
 require 'imw/utils/paths'
 require 'imw/workflow/scaffold'
 require 'imw/extract'
-# require 'fileutils'; include FileUtils::Verbose
+require 'fileutils'; include FileUtils #::Verbose
 require 'YAML'
 include IMW
 
@@ -16,18 +16,21 @@ scaffold_dataset 'weather/ncdc/stations'
 #
 us_stns = YAML.load(File.open(path_to(:fixd, 'ncdc_weather_stations-US.yaml')))
 
+SEARCH_SUBSTRING = 'ci1' # |ch2|ci1|ci2'
 good_stns = []
-[2008].each do |year|
+[2007].each do |year|
+  mkdir_p "temp/noaa/#{year}"
   us_stns[1..10000].each do |stn|
     wban = stn[:WBAN_weatherstation_code]
     usaf = stn[:USAF_weatherstation_code]
     file = "#{year}/#{usaf}-#{wban}-#{year}"
     if File.exist?("ripd/noaa/#{file}.gz")
-      has_humidity_measurements = `zcat ripd/noaa/#{file}.gz | grep -qi SA && echo 1`
+      has_humidity_measurements = `zcat ripd/noaa/#{file}.gz | egrep -qi '#{SEARCH_SUBSTRING}' && echo 1`
       if has_humidity_measurements.chomp != ''
         (stn[:datafiles]||={})[:year] = file
         good_stns << stn
         puts file
+        `zcat ripd/noaa/#{file}.gz > "temp/noaa/#{file}".flat.txt`
       end
     end
   end
