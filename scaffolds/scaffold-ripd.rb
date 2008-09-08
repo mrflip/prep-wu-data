4de9722e5b3165719d380eb96795f053961e7a32
Philip (flip) Kromer <flip@infochimps.org>
2008-09-07 21:53:52 -0500

    OK Dhruv Have at it you crazy bastard

--- scaffolds/scaffold-ripd.rb
+++ scaffolds/scaffold-ripd.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'erubis'
-require 'fileutils'; include FileUtils::Verbose
+require 'fileutils'; include FileUtils
 
-input = File.read 'template.icss.yaml.erb'
+input = File.read '../icss/template.icss.yaml.erb'
 eruby = Erubis::Eruby.new(input)
 
 Dir['/data/ripd/*'].find_all{|d| File.directory? d}.each do |d|
   d = File.basename d
+  if File.exists? "ripd-edited/#{d}/#{d}.icss.yaml" then puts "Skipping #{d}" ; next ; end
   icss_file = "ripd-scaffold/#{d}/#{d}.icss.yaml"
   mkdir_p File.dirname(icss_file)
   File.open(icss_file, 'w') do |f|
