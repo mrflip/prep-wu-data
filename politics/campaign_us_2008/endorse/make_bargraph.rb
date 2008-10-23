13ece802d639488876a34d4343e96878055e4247
Philip (flip) Kromer <flip@infochimps.org>
2008-10-22 23:25:52 -0500

    Bargraph

---
+++ politics/campaign_us_2008/endorse/make_bargraph.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'yaml'
+require 'xmlsimple'
+require 'imw/utils/extensions/core'
+#
+require 'state_abbreviations'
+require 'newspaper_mapping'
+require 'cities_mapping'
+require 'map_projection'
+
+class Endorsement < Struct.new(
+  :prez, :prev, :rank, :circ, :daily, :sun, :lat, :lng, :st, :city, :paper,
+    :movement, :prez04,
+    :all_rank
+  )
+  def initialize(*args)
+    super *args
+    [:circ, :daily, :sun, :movement].each{|attr| self[attr] = self[attr].to_i }
+    [:lat, :lng                    ].each{|attr| self[attr] = self[attr].to_f }
+    self.movement ||= 0
+  end
+
+
+  def self.load(tsv_file)
+    File.open(tsv_file).readlines.map do |line|
+      self.new( *line.split(/\t/) )
+    end[1..-1]
+  end
+end
+
+PROCESS_DATE = '20081020'
+tsv_in_filename         = "fixd/endorsements-cooked-#{PROCESS_DATE}.tsv"
+xml_bargraph_filename   = "fixd/endorsements-bargraph.xml"
+
+#
+# Load file
+#
+endorsements = Endorsement.load(tsv_in_filename)
+
+#
+# Create sorted series data
+#
+endorsements = endorsements.reject{|e| e.circ==0 }.sort_by{|e| e.circ}[0..-1]
+endorsements.each_with_index{|e,i| e.all_rank = i }
+
+BARGRAPH_COLORS = {
+   3 => '#4E94E1',  2 => '#A1BDDF',  1 => '#A1BDDF', 0 => '#999999',
+  -3 => '#E45353', -2 => '#E1A0A0', -1 => '#E1A0A0' }
+def make_bargraph_point e
+
+  { 'content' => (case e.prez when 'Obama' then -e.circ    when 'McCain' then e.circ   else 0  end),
+    'xid'     => e.all_rank,
+    'color'   => BARGRAPH_COLORS[e.movement],
+    'description' => "#{e.paper}<br/>Circulation #{e.circ}<br>2008:#{e.prez}, 2004:#{(e.prez04=='' ? 'none' : e.prez04 )}",
+  }
+end
+
+endorsements_bargraph = { 'chart' => {
+    'series' => { 'value' => (0..endorsements.length).map{|i| { 'content' => i, 'xid' => i }}, },
+    'graphs' => { 'graph' => [
+        {'gid' => 1, 'value' => endorsements.map{|e| make_bargraph_point(e)  } }
+      ]
+    }
+  }}
+
+
+#
+# write data file
+#
+File.open(xml_bargraph_filename, 'w') do |f|
+  f << XmlSimple.xml_out(endorsements_bargraph, 'KeepRoot' => true)
+end
