590c190cfa0f741e6973b51ae50c3fed4233b685
Philip (flip) Kromer <flip@infochimps.org>
2008-11-23 01:45:48 -0600

    Starting new scrape strategy

---
+++ social/network/twitter_friends/twitter_scrape_public_timeline.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+poolpath="social/network/twitter_friends/public_timeline"
+datadir="/data/rawd/${poolpath}"
+logdir="/data/log/${poolpath}"
+url="http://twitter.com/statuses/public_timeline.rss"
+waittime=3
+
+mkdir -p $datadir
+cd       $datadir
+for (( i=0 ; 1 ; true )) ; do
+    filename=$datadir/`date +'%Y%m/%d/%H%M/public_timeline-%Y%m%d-%H%M%S.rss'`
+    logname=$logdir/`date +'twitter_public_timeline-%Y%m%d.log'`
+    echo wget -nc -nv -a $logname -O $filename
+    sleep $waittime
+    true
+done
