108ef16bb8231f3ab7786df4bbe3da94d1f2cec0
Philip (flip) Kromer <flip@infochimps.org>
2008-12-01 02:30:53 -0600

    hadoop bundler

---
+++ social/network/twitter_friends/twitter_bundle_json.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'imw' ; include IMW
+require 'twitter_profile_model'
+require 'fileutils'; include FileUtils
+as_dset __FILE__
+
+DIR_TO_RESOURCE =  {
+  'users/show'         => :raw_userinfo,
+  'statuses/friends'   => :raw_friends,
+  'statuses/followers' => :raw_followers
+}
+def key_from_filename filename
+  timestamp = File.mtime(filename).strftime("%Y%m%d-%H%M%S")
+  if m = %r{_com/_tw/com.twitter/(\w+/\w+)/_(..?)/(\w+?)\.json(?:%3Fpage%3D(\d+))?$}.match(filename)
+    dir, prefix, screen_name, page = m.captures
+    [ DIR_TO_RESOURCE[dir], screen_name, page, timestamp ]
+  else
+    raise "Can't grok filename #{filename}"
+  end
+end
+
+Dir["/data/ripd/_com/_tw/com.twitter/*/*/*"].each do |dir|
+  m = %r{^.*/com\.twitter/(\w+/\w+)/_(..?)$}.match(dir) or raise("can't grok #{dir}")
+  segment, prefix = m.captures;
+  resource = DIR_TO_RESOURCE[segment]; prefix = prefix.downcase
+  mkdir_p("/data/rawd/social/network/twitter_friends/#{resource}")
+  dump_filename = "/data/rawd/social/network/twitter_friends/#{resource}/#{resource}-#{prefix}-raw.tsv"
+  next if File.exist?(dump_filename)
+  File.open(dump_filename, "w") do |f|
+    $stderr.puts "#{Time.now}\tScraping #{resource} - #{prefix}*"
+    Dir[dir+'/*'].each do |filename|
+      next unless File.size(filename) > 0
+      resource, screen_name, page = key_from_filename filename
+      key = [resource, screen_name, page].join("-")
+      f << "#{key}\t#{File.read(filename)}\n"
+    end
+  end
+end
