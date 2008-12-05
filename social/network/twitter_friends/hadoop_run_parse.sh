d8ada474edb0a5ff80ddb387fd56a3e33ee4ccd3
Philip (flip) Kromer <flip@infochimps.org>
2008-12-05 01:55:33 -0600

    MysQL import stuff

--- social/network/twitter_friends/hadoop_run_parse.sh
+++ social/network/twitter_friends/hadoop_run_parse.sh
@@ -4,7 +4,7 @@
 hadoop jar /home/flip/hadoop/h/contrib/streaming/hadoop-*-streaming.jar			\
     -mapper  /home/flip/ics/pool/social/network/twitter_friends/hadoop_parse_json.rb	\
     -reducer /home/flip/ics/pool/social/network/twitter_friends/hadoop_parse_reduce.rb	\
-    -input   /user/flip/rawd/social/network/twitter_friends-20081204/'*'	 	\
+    -input   /user/flip/rawd/social/network/twitter_friends-20081205b/'*'	 	\
     -output  "$1"									\
     -file    hadoop_utils.rb								\
     -file    twitter_autourl.rb
