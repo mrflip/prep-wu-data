cc9a209f01f116f8b97374418d5b8416be2951e2
Philip (flip) Kromer <flip@infochimps.org>
2008-12-11 20:44:42 -0600

    tuning scrape

--- social/network/twitter_friends/twitter_scrape_json_2.rb
+++ social/network/twitter_friends/twitter_scrape_json_2.rb
@@ -8,6 +8,7 @@ as_dset __FILE__
 
 # Get the password -- keep this file .gitignore'd obvs.
 require File.dirname(__FILE__)+'/twitter_pass.rb'
+IMW.log.level = Logger::INFO
 
 class TwitterScrapeFile
   include ScrapeFile
@@ -34,20 +35,24 @@ class TwitterScrapeFile
   def rip_uri
     "http://twitter.com/#{resource_path}/#{screen_name}.json?page=#{page}"
   end
+
 end
 
 USERNAMES_FILE = 'fixd/dump/user_names_by_followers_count.tsv'
 # USERNAMES_FILE = '/tmp/foo_users.tsv'
-exists, not_exists = [0,0]
+exists, not_exists, lines = [0,0,0]
+SKIP_LINES = 10000
 File.open(USERNAMES_FILE).each do |line|
-  line.chomp!
+  line.chomp!; lines += 1
+  next if lines < SKIP_LINES
   screen_name, *rest = line.split(/\t/); next unless screen_name
-  track_count(:users, 1000)
-  [:user ].each do |context|
+  [:followers ].each do |context|
+    track_count(:fetches, 1000)
     scrape_file = TwitterScrapeFile.new(screen_name, context, 1)
     if scrape_file.exists? then exists += 1  else  not_exists += 1 end
     puts "Exists\t#{exists}\tNot Exists\t#{not_exists}" if (exists % 4000 == 0)
-    success = scrape_file.wget :http_user => TWITTER_USERNAME, :http_passwd => TWITTER_PASSWD, :sleep_time => 0
+    success = scrape_file.wget :http_user => TWITTER_USERNAME, :http_passwd => TWITTER_PASSWD,
+      :sleep_time => 0
     warn "No yuo on #{screen_name}: #{scrape_file.result_status}" unless success
   end
 end
