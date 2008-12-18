b7d98c01d6f41d77075d697d1270b905a9394244
Philip (flip) Kromer <flip@infochimps.org>
2008-12-18 13:41:14 -0600

    made public timeline scraper work

--- social/network/twitter_friends/twitter_scrape_public_timeline.sh
+++ social/network/twitter_friends/twitter_scrape_public_timeline.sh
@@ -3,23 +3,23 @@ poolpath="social/network/twitter_friends/public_timeline"
 datadir="/data/rawd/${poolpath}"
 logdir="/data/log/${poolpath}"
 
-public_url="http://twitter.com/statuses/public_timeline_partners_nrab481.xml"
+public_url=`ruby -e 'require "twitter_pass"; puts TWITTER_DATAMINING_FEED_URL'`
 trends_url="http://search.twitter.com/trends.json"
-waittime=59
+waittime=58
 
 mkdir -p $logdir
 mkdir -p $datadir
 cd       $datadir
 for (( i=0 ; 1 ; true )) ; do
     logname=$logdir/`date +'twitter_public_timeline-%Y%m%d.log'`
-    datedir=$datadir/`date +'%Y%m/%d/%H'`
-    datetime=`date +'%Y%m%d-%H%M%S'`
     mkdir -p `dirname $logname`
-    mkdir -p `dirname $datedir`
-    public_filename=
+    datedir=$datadir/`date +'%Y%m/%d/%H'`
+    mkdir -p $datedir
+    datetime=`date +'%Y%m%d-%H%M%S'`
     wget -nc -nv -a $logname $public_url -O $datedir/public_timeline-$datetime.json
-    wget -nc -nv -a $logname $trends_url -O $datedir/trends-$datetime.json
+    # wget -nc -nv -a $logname $trends_url -O $datedir/trends-$datetime.json
     sleep $waittime
     true
 done
 
+ 
