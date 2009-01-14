081a2d42edcdf58c596743700e4015c09e0679b3
Philip (flip) Kromer <flip@infochimps.org>
2009-01-14 01:20:50 -0600

    Fixing comments

--- social/network/twitter_friends/rdfify.rb
+++ social/network/twitter_friends/rdfify.rb
@@ -23,30 +23,13 @@ module Rdfify
         puts [subj, obj, timestamp, pred].join("\t")
       end
     end
-
-    #
-    # Skip bogus records
-    #
-    def itemize line
-      return if line =~ /^(?:bogus|bad_record)/
-      super line
-    end
   end
 
   # #
   # # We'd like to extract the *latest* value for each property
   # #
-  # #
-  # # Mapper emits
-  # #  subject    predicate       timestamp       object
-  # #
-  # # We sort on the first three fields, guaranteeing that the last value for a
-  # # given subject+predicate is the latest seen.  Discarding all but the last
-  # # uniqifies immutable properties and gives the most up-to-date for mutable
-  # # properties
-  # #
   # # Note especially that the scraped_at value thus holds the 'last time seen'
-  # # for the **subject** (and not for the subject-predicate).  For example, our
+  # # for the **subject** (that is, the attribute and not its object).  For example, our
   # # last scrape might have yielded a TwitterUserPartial giving a new
   # # followers_count, while the statuses_count came from an earlier TwitterUser
   # # sighting.  The scraped_at value gives only the latest (partial user) date.
@@ -55,22 +38,6 @@ module Rdfify
   # # them disappear.
   # #
   # class Reducer < Hadoop::UniqByLastReducer
-  #   attr_accessor :final_value
-  #
-  #   #
-  #   # Key on subject + predicate
-  #   #
-  #   def get_key subj, pred, timestamp, obj
-  #     [subj, pred] # FIXME -- this kills all but one <user follows user_b>
-  #   end
-  #   #
-  #   # Emit the last-seen (latest) value
-  #   # unswapping the timestamp and predicate
-  #   #
-  #   def finalize
-  #     subj, obj, timestamp, pred = final_value
-  #     puts TwitterFriends::TwitterRdf.rdf_triple(subj, obj, pred, timestamp)
-  #   end
   # end
 
   class Script < Hadoop::Script
@@ -86,7 +53,6 @@ end
 Rdfify::Script.new(
   Rdfify::Mapper,
 
-  # Rdfify::Reducer             # !!!!!!!!!!!!!!!!!!! Nil reducer !!!!!!!!!!!!!!!!!!!!!!!!!!!
-  nil
+  nil # !!!!!!!!!!! note removed reducer, just straight uniq for nows.
 
   ).run
