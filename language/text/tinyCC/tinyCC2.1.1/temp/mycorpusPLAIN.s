0aeb41bd2e7b17fbeb61a4a5e5d3b4d258a4862f
Philip (flip) Kromer <flip@infochimps.org>
2009-05-17 23:17:30 -0500

    Ran tinyCC, saw results. That was fun.

---
+++ language/text/tinyCC/tinyCC2.1.1/temp/mycorpusPLAIN.s
@@ -0,0 +1,117 @@
+1	1065813774	Wortschatz - International Portal
+2	381208381	Leipzig Corpora Collection Download Page
+3	1343048081	The Leipzig Corpora Collection presents corpora in different languages using the same format and comparable sources.
+4	1088234246	The corpora are ready to use with the Corpus Browser.
+5	2026488912	Moreover, all data are available as plain text and as MySQL database tables for various applications.
+6	41448654	They are intended both for scientific use by the corpus linguist as well as for applications such as knowledge extraction programs.
+7	55009798	The corpora are identical in format and similar in size and content.
+8	918192789	They contain randomly selected sentences in the language of the corpus and are available in sizes of 100,000 sentences, 300,000 sentences, 1 million sentences etc..
+9	719801784	The sources are either newspaper texts or texts randomly collected from the web.
+10	1278029970	The texts are split into sentences.
+11	307432018	Non-sentences and foreign language material was removed.
+12	1630681087	Because the information which words co-occur with each other is useful for many applications, these data ware precomputed and included as well.
+13	286846381	For each word, the most significant words appearing
+14	1057518620	as immediate left neighbor
+15	518613777	as immediate right neighbor
+16	1644040979	anywhere within the same sentence
+17	969239155	are given.
+18	800335527	The quality of such co-occurrence increases with the corpus size, so we refer to forthcoming larger corpora.
+19	101786365	Reference:
+20	1376599622	Quasthoff, U.; M. Richter; C. Biemann: Corpus Portal for Search in Monolingual Corpora, Proceedings of the fifth international conference on Language Resources and Evaluation, LREC 2006, Genoa, pp. 1799-1802
+21	1530407410	Download: Corpusportal The Corpus Browser
+22	155413178	Download Corpus Browser : Version 1.0 (May 2006) (Filesize: ~2.1mb)
+23	1770260530	Download Documentation : Version 1.0 (May 2006) (Filesize: ~1.3mb) Download Corpora
+24	1200685415	To use the above browser, select one or more languages and sizes an download the corresponding MySQL data files.
+25	174283825	These data are also available as plain text for the convenience of the user.
+26	1233306741	They are not necessary for the browser.
+27	1718034300	Language
+28	1776190125	Corpus Size
+29	621718187	MySQL Data Files
+30	2095588006	Plain Text Files
+31	1536125632	Catalan
+32	2549592	100k
+33	2650898	300k
+34	2549592	100k
+35	2650898	300k
+36	607935331	Danish
+37	2549592	100k
+38	2549592	100k
+39	133531920	Dutch
+40	2549592	100k
+41	2549592	100k
+42	178020974	English
+43	2549592	100k
+44	2650898	300k
+45	1890	1M
+46	2549592	100k
+47	2650898	300k
+48	1890	1M
+49	1152095047	Estonian
+50	2549592	100k
+51	2650898	300k
+52	2549592	100k
+53	2650898	300k
+54	1884719265	Finnish
+55	2549592	100k
+56	2549592	100k
+57	778034358	French
+58	2549592	100k
+59	2549592	100k
+60	823671274	German
+61	2549592	100k
+62	2650898	300k
+63	1890	1M
+64	1964	3M
+65	2549592	100k
+66	2650898	300k
+67	1890	1M
+68	1964	3M
+69	2039157162	Italian
+70	2549592	100k
+71	2650898	300k
+72	2549592	100k
+73	2650898	300k
+74	1818834051	Japanese
+75	2549592	100k
+76	2549592	100k
+77	1119777760	Korean
+78	2549592	100k
+79	2650898	300k
+80	2549592	100k
+81	2650898	300k
+82	391135574	Norwegian
+83	2549592	100k
+84	2650898	300k
+85	2549592	100k
+86	2650898	300k
+87	1827904870	Sorbian
+88	2549592	100k
+89	2549592	100k
+90	1936572897	Swedish
+91	2549592	100k
+92	2549592	100k
+93	515184262	Turkish
+94	2549592	100k
+95	2549592	100k
+96	1761502110	Conditions of use
+97	584469224	The Leipzig Corpora Collection contain text from publicly accessible sources.
+98	1407514571	All data have been processed automatically so that it is not possible to reconstruct the original source texts.
+99	2033493216	The corpora are protected by copyright.
+100	437516285	They are made available on the condition that they may be used for scientific purposes only and not passed on to third parties.
+101	811202753	Any use of the data must be duly documented and referenced.
+102	2104457047	Commercial use of the data requires the prior written consent of the Leipzig University department for Natural Language Processing.
+103	1133651697	Disclaimer
+104	1123137920	The Leipzig Corpora Collection have been processed automatically from publicly accessible sources based on the outlined methodology without considering in detail the content of the contained text.
+105	2058724663	No responsibility is taken for the content of the data.
+106	1871954681	In particular, the views and opinions expressed in specific parts of the data remain exclusively with the authors.
+107	1896512957	For each word, the list of words that significantly co-occur with that word are computed on the basis of the available text and neither express a general fact of language nor the particular view of the Leipzig University department for Natural Language Processing.
+108	1564924759	Back to Wortschatz
+109	1440850792	LCC-Download pageLCC-Download page Wortschatz - International Portal - Terms Of Usage
+110	1056184947	terms of usage
+111	438915028	Any data provided by Projekt Deutscher Wortschatz are subject to copyright.
+112	1152081594	Permission for use is granted free of charge solely for non-commercial personal and scientific purposes.
+113	1028942917	Any use that exceeds the means of query provided by the WWW-Interface, any automated queries and any commercial use, processing or permanent storage of the data obtained is forbidden without explicit written permission by the copyright owner.
+114	1564924759	Back to Wortschatz
+115	294943169	????? ??? ??????? ???? ??????????????? -- ?? ???????? ??? ??????????????? ??????, ?????????? ??????????? ?? ???????? ? ????? ????????? ????????.
+116	1116290455	Das ist ein Test, ob ASCII-Text gleich bleibt.
+117	1836481261	Werden ?ml?uhalten?
