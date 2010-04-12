0a08ce8fb0bf94d00b244357439dd9768b37b6a1
Carl Knutson <carl@infochimps.org>
2010-04-12 17:09:45 -0500

    Updated regexp

--- social/network/twitter/simple_filters/get_stock_tweets.rb
+++ social/network/twitter/simple_filters/get_stock_tweets.rb
@@ -8,11 +8,15 @@ require 'wuclan/twitter/model/token'
 module StockTweets
   class Mapper < Wukong::Streamer::StructStreamer
  
- 
+     #
+     #  This separates tweets with stock symbols which have started using the convention of:
+     #     $SYMBOL or just $$ to talk about money related things
+     #  The website StockTwits http://stocktwits.com has built its website and tools around this convention.
+     #
      def process obj, *_, &block
        return unless (obj.is_a?(Tweet) || obj.is_a?(SearchTweet))
-       if obj.text =~ /\$\$|\$[a-zA-Z\:\^\.\_]+/
-         obj.text.scan(/\$\$|\$[a-zA-Z\:\^\.\_]+/).each do |stock_symbol|
+       if obj.text =~ /\$\$+|\$[a-zA-Z\:\^\.\_]+/
+         obj.text.scan(/\$\$+|\$[a-zA-Z\:\^\.\_]+/).each do |stock_symbol|
            yield ['stock_tweet',stock_symbol,obj.text]
          end
        end
