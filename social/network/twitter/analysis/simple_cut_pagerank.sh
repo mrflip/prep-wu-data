200683905b6a59b101da996bf8762ef83e1ee1b7
Philip (flip) Kromer <flip@infochimps.org>
2010-04-15 13:45:08 +0000

    fixed naming oopsie on a script to cut first and second fields from pagerank, TODO: this script and joining pagerank with profiles should be merged as one pig script?

--- social/network/twitter/analysis/simple_cut_pagerank.sh
+++ social/network/twitter/analysis/simple_cut_pagerank.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+B1;2c#!/usr/bin/env bash
 
 script_dir=$(readlink -f `dirname $0`)
 
-hdp-rm -r /data/rawd/social/network/twitter/pagerank/a_follows_b_pagerank/pagerank_only
+hdp-rm -r /data/rawd/social/network/twitter/pagerank/a_replies_b_pagerank/pagerank_only
 /usr/lib/hadoop/bin/hadoop jar /usr/lib/hadoop/contrib/streaming/hadoop-*-streaming.jar       \
   -D           mapred.reduce.tasks=200                                                       \
   -D           stream.num.map.output.key.fields="2"                                          \
   -mapper      "/usr/bin/cut -f1,2"                                                      \
   -reducer     "/usr/bin/uniq"                                                               \
-  -input       "/data/rawd/social/network/twitter/pagerank/a_follows_b_pagerank/pagerank_graph_10" \
-  -output      "/data/rawd/social/network/twitter/pagerank/a_follows_b_pagerank/pagerank_only"                             \
+  -input       "/data/rawd/social/network/twitter/pagerank/a_replies_b_pagerank/pagerank_graph_010" \
+  -output      "/data/rawd/social/network/twitter/pagerank/a_replies_b_pagerank/pagerank_only"                             \
   -cmdenv       LC_ALL=C
