0a56655a8e59ac589c5b49306232d39818786fdb
Carl Knutson <carl@infochimps.org>
2010-05-13 15:43:09 -0500

    Changed to record streamer, calculated account age.

--- social/network/twitter/tweet_metrics/replies_json.rb
+++ social/network/twitter/tweet_metrics/replies_json.rb
@@ -5,15 +5,18 @@ require 'wukong'                       ; include Wukong
 require '../utils/json/tsv_to_json'    ; include TSVtoJSON
 
 Settings.resolve!
-Settings.json_keys = "screen_name,id,statuses,replies_out,replies_in,scraped_at,created_at"
+Settings.json_keys = "screen_name,id,statuses,replies_out,replies_in,account_age"
 
 module ReplyMetricsJSON
-  class Mapper < Wukong::Streamer::StructStreamer
+  class Mapper < Wukong::Streamer::RecordStreamer
 
 
-    def process screen_name, id, statuses, replies_out, replies_in, scraped_at, created_at, *_, &block
-      row = [screen_name,id,statuses,replies_out,replies_in,scraped_at,created_at]
-      yield [screen_name, id, TSVtoJSON::into_json(row)].flatten
+    def process *line, &block
+      return if line[6].nil?
+      created = Time.gm(line[6][0..3].to_i,line[6][4..5].to_i,line[6][6..7].to_i)
+      scraped = Time.gm(line[5][0..3].to_i,line[5][4..5].to_i,line[5][6..7].to_i)
+      line[5] = ((scraped - created)/(24*60*60)).to_i
+      yield [line[0..1], TSVtoJSON::into_json(line)].flatten
     end
     
   end
