a20fab49a4b16ad335690321518c19319cf5eae6
Carl Knutson <carl@infochimps.org>
2010-05-20 19:50:51 +0000

    Changed integers to be integers instead of strings.

--- social/network/twitter/tweet_metrics/replies_json.rb
+++ social/network/twitter/tweet_metrics/replies_json.rb
@@ -18,6 +18,10 @@ module ReplyMetricsJSON
       created = Time.gm(line[6][0..3].to_i,line[6][4..5].to_i,line[6][6..7].to_i)
       scraped = Time.gm(line[5][0..3].to_i,line[5][4..5].to_i,line[5][6..7].to_i)
       line[5] = ((scraped - created)/(24*60*60)).to_i
+      line[1] = line[1].to_i
+      line[2] = line[2].to_i
+      line[3] = line[3].to_i
+      line[4] = line[4].to_i
       yield [line[0..1], TSVtoJSON::into_json(line)].flatten
     end
     
