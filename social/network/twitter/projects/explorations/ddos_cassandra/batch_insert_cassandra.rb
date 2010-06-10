167f7c009ec03ceb1e63a8f47b2c10031222d437
Jacob <jacob.a.perkins@gmail.com>
2010-06-10 12:07:53 -0500

    edited batch insertion script to work with zaius

--- social/network/twitter/projects/explorations/ddos_cassandra/batch_insert_cassandra.rb
+++ social/network/twitter/projects/explorations/ddos_cassandra/batch_insert_cassandra.rb
@@ -3,21 +3,27 @@ require 'rubygems'
 require 'wukong'    ; include Wukong
 require 'cassandra' ; include Cassandra::Constants
 
+# Address       Status     Load          Range                                      Ring
+#                                        oCS61vbuHm16Ebet                           
+# 10.244.191.178Up         554 bytes     7I3nWSCieE5suAwJ                           |<--|
+# 10.243.19.223 Up         1.08 KB       Sg8HIJxekRRRMMnc                           |   ^
+# 10.243.17.219 Up         829 bytes     UQcJBHiXEk5sijAV                           v   |
+# 10.245.70.85  Up         829 bytes     lsAOnAOUhCh6NU09                           |   ^
+# 10.244.206.241Up         554 bytes     oCS61vbuHm16Ebet                           |-->|
+  
 class CassandraBatchMapper < Wukong::Streamer::Base
   attr_accessor :batch_count, :batch_record_count
-  CASSANDRA_DB_SEEDS = %w[10.196.225.203 10.196.193.219 10.196.227.79 10.196.227.159 10.196.199.47 10.196.225.171 10.196.162.15].map{ |s| s.to_s+':9160'}
+  CASSANDRA_DB_SEEDS = %w[10.244.191.178 10.243.19.223 10.243.17.219 10.245.70.85 10.244.206.241].map{ |s| s.to_s+':9160'}
   BATCH_SIZE = 100
 
   def cassandra_db
-    # @cassandra_db ||= Cassandra.new('Cruft', CASSANDRA_DB_SEEDS)
-    @cassandra_db ||= Cassandra.new('Cruft')
+    @cassandra_db ||= Cassandra.new('test_a', CASSANDRA_DB_SEEDS)
   end
   
   def initialize *args
     super *args
     self.batch_count = 0
     self.batch_record_count = 0
-    @words = File.open('/usr/share/dict/american-english-insane')
   end
   
   def stream
@@ -35,8 +41,8 @@ class CassandraBatchMapper < Wukong::Streamer::Base
     end
   end
 
-  def process *args, &blk
-    insert_cruft do |word|
+  def process line, &blk
+    insert_cruft(line) do |word|
     # read_cruft do |word|
       yield word
     end
@@ -60,9 +66,8 @@ class CassandraBatchMapper < Wukong::Streamer::Base
     self.batch_record_count < BATCH_SIZE
   end
 
-  def insert_cruft &blk
-    word = @words.gets.strip
-    cassandra_db.insert(:OhBaby, word, "time" => Time.now.to_i.to_s) unless word.blank?
+  def insert_cruft word, &blk
+    cassandra_db.insert(:words, word.strip, "time" => Time.now.to_i.to_s) unless word.blank?
     yield word
   end
 
