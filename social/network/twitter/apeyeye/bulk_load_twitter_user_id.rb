5871efe1994d253abacd7b790d9a4f3fc965eb62
Jacob <jacob.a.perkins@gmail.com>
2010-06-18 12:17:36 -0500

    got rid of periodic logger calls for now

--- social/network/twitter/apeyeye/bulk_load_twitter_user_id.rb
+++ social/network/twitter/apeyeye/bulk_load_twitter_user_id.rb
@@ -6,13 +6,20 @@ require 'configliere' ; require 'configliere/commandline'
 require 'wukong/keystore/tyrant_db' ; include TokyoDbConnection
 require File.dirname(__FILE__)+'/periodic_logger'
 
+ID_LOG_INTERVAL = 1000
+
 class TwitterIdsBulkLoader < Wukong::Streamer::RecordStreamer
 
   UID_DB = TyrantDb.new(:user_ids)
   SN_DB  = TyrantDb.new(:screen_names)
   SID_DB = TyrantDb.new(:search_ids)
+
+  def initialize *args
+    super(*args)
+    @iter = 0
+  end
   
-  def process rsrc, user_id, scraped_at, screen_name, is_protected, followers_count, friends_count, statuses_count, favourites_count, created_at, search_id, is_full, *_, &block
+  def process rsrc, user_id, scraped_at, screen_name, is_protected, followers_count, friends_count, statuses_count, favourites_count, created_at, search_id, is_full, *_, &block    
     user_id     = nil if user_id.empty?
     screen_name = nil if screen_name.empty?
     search_id   = nil if search_id.empty?
@@ -24,10 +31,11 @@ class TwitterIdsBulkLoader < Wukong::Streamer::RecordStreamer
     # if user_id     then ; UID_DB[user_id]             or yield ['user_id_missing', user_id] ; end
     # if screen_name then ; SN_DB[screen_name.downcase] or yield ['screen_name_missing', screen_name] ; end
     # if search_id   then ; SID_DB[search_id]           or yield ['search_id_missing', search_id] ; end
-    log.periodically do
-      yield  log.progress("%7d"%user_id, "%7d"%screen_name)
-      $stderr.puts log.progress("%7d"%user_id, "%7d"%screen_name)
+    if( @iter % ID_LOG_INTERVAL == 0 )
+      $stderr.puts "user_id = #{user_id}, screen_name = #{screen_name}"
+      yield [user_id, screen_name]
     end
+    @iter += 1
   end
 
 end
