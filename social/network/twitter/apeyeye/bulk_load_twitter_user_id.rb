d8fcb2f764e0a754babaec8845effc4bd5d7fed4
Philip (flip) Kromer <flip@infochimps.org>
2010-06-18 18:55:54 +0000

    Oops on handling nils in bulk_load_twitter_user_id.rb

--- social/network/twitter/apeyeye/bulk_load_twitter_user_id.rb
+++ social/network/twitter/apeyeye/bulk_load_twitter_user_id.rb
@@ -21,18 +21,18 @@ class TwitterIdsBulkLoader < Wukong::Streamer::RecordStreamer
   end
 
   def process rsrc, user_id, scraped_at, screen_name, is_protected, followers_count, friends_count, statuses_count, favourites_count, created_at, search_id, is_full, *_, &block
-    user_id     = (user_id.empty?     ? nil : user_id )
-    screen_name = (screen_name.empty? ? nil : screen_name.downcase)
-    search_id   = (search_id.empty?   ? nil : search_id )
+    user_id     = (user_id.blank?     ? nil : user_id )
+    screen_name = (screen_name.blank? ? nil : screen_name.downcase)
+    search_id   = (search_id.blank?   ? nil : search_id )
 
     if Settings[:read]
-      if user_id     then ; UID_DB[user_id]             or yield ['user_id_missing', user_id] ; end
-      if screen_name then ; SN_DB[screen_name.downcase] or yield ['screen_name_missing', screen_name] ; end
-      if search_id   then ; SID_DB[search_id]           or yield ['search_id_missing', search_id] ; end
+      if user_id     then ; UID_DB[user_id]    or yield ['user_id_missing', user_id] ; end
+      if screen_name then ; SN_DB[screen_name] or yield ['screen_name_missing', screen_name] ; end
+      if search_id   then ; SID_DB[search_id]  or yield ['search_id_missing', search_id] ; end
     else
-      UID_DB.insert(user_id,              screen_name.downcase) if user_id
-      SN_DB.insert( screen_name.downcase, user_id.to_s)         if screen_name
-      SID_DB.insert(search_id,            screen_name.downcase) if search_id
+      UID_DB.insert(user_id,     screen_name)  if user_id     && screen_name
+      SN_DB.insert( screen_name, user_id)      if screen_name && user_id
+      SID_DB.insert(search_id,   screen_name)  if search_id   && screen_name
     end
 
     @log.periodically do
