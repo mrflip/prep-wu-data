d464e8591a6452ff0bbff474ea2d2fac28ea846a
Philip (flip) Kromer <flip@infochimps.org>
2010-06-19 06:52:46 +0000

    removed unneccessary constant in id bulk loader

--- social/network/twitter/apeyeye/bulk_load_twitter_user_id.rb
+++ social/network/twitter/apeyeye/bulk_load_twitter_user_id.rb
@@ -6,10 +6,7 @@ require 'configliere' ; require 'configliere/commandline'
 require 'wukong/keystore/tyrant_db' ; include TokyoDbConnection
 require File.dirname(__FILE__)+'/periodic_logger'
 
-ID_LOG_INTERVAL = 1000
-
 class TwitterIdsBulkLoader < Wukong::Streamer::RecordStreamer
-
   UID_DB = TyrantDb.new(:user_ids)
   SN_DB  = TyrantDb.new(:screen_names)
   SID_DB = TyrantDb.new(:search_ids)
@@ -26,9 +23,9 @@ class TwitterIdsBulkLoader < Wukong::Streamer::RecordStreamer
     search_id   = (search_id.blank?   ? nil : search_id )
 
     if Settings[:read]
-      if user_id     then ; UID_DB[user_id]    or yield ['user_id_missing', user_id] ; end
+      if user_id     then ; UID_DB[user_id]    or yield ['user_id_missing', user_id]         ; end
       if screen_name then ; SN_DB[screen_name] or yield ['screen_name_missing', screen_name] ; end
-      if search_id   then ; SID_DB[search_id]  or yield ['search_id_missing', search_id] ; end
+      if search_id   then ; SID_DB[search_id]  or yield ['search_id_missing', search_id]     ; end
     else
       UID_DB.insert(user_id,     screen_name)  if user_id     && screen_name
       SN_DB.insert( screen_name, user_id)      if screen_name && user_id
