1afa9ca88fdf661de5374e788d8bf6ec125ba4c5
Jacob <jacob.a.perkins@gmail.com>
2010-06-22 19:50:48 -0500

    fixd a bug in uniq all script, added skeleton of trstrank approximator

--- social/network/twitter/base/parse/uniq_all.rb
+++ social/network/twitter/base/parse/uniq_all.rb
@@ -12,11 +12,11 @@ objects   = ARGV[1]
 %w[ twitter_user twitter_user_partial twitter_user_profile twitter_user_location twitter_user_style ].each do |object|
   inputdir  = File.join(unspliced, object)
   outputdir = File.join(objects, object)
-  system %Q{echo ./last_seen_state.rb --rm --run #{inputdir} #{outputdir}}
+  system %Q{echo ./last_seen_state.rb --rm --run #{inputdir} #{outputdir} }
 end
 
-%w[ a_follows_b geo delete_tweet tweet twitter_user_search_id ].each do |object|
+%w[ a_follows_b geo delete_tweet tweet-noid twitter_user_search_id ].each do |object|
   inputdir  = File.join(unspliced, object)
   outputdir = File.join(objects, object)
-  system %Q{echo hdp-stream #{inputdir} #{outputdir} `which cat` `which uniq` 3 2}
+  system %Q{echo hdp-stream #{inputdir} #{outputdir} `which cat` `which uniq` 2 3 -jobconf mapred.map.tasks.speculative.execution=false}
 end
