5aea9761c5693509ed6ae8de0a141e594ca9e1e8
Jacob <jacob.a.perkins@gmail.com>
2010-06-29 11:25:12 -0500

    stupidly simple script to jsonize trstrank fields and serialize to disk

--- social/network/twitter/projects/trstme/jsonize_trstrank.rb
+++ social/network/twitter/projects/trstme/jsonize_trstrank.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'wukong'
+require 'json'
 
 class Mapper < Wukong::Streamer::RecordStreamer
   
-  def process screen_name, user_id, rank, &blk
+  def process screen_name, user_id, raw, scaled, &blk
+    yield [screen_name, user_id, jsonize(scaled)]
   end
 
-  def jsonize
+  def jsonize scaled
+    hsh = {:trstrank => scaled}
+    hsh.to_json
   end
   
 end
