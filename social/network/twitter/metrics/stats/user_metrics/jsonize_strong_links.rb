2e97f7d42407f96ee86a8f66d21577bc1445430c
Jacob <jacob.a.perkins@gmail.com>
2010-07-27 15:05:49 -0500

    script to jsonize strong links

---
+++ social/network/twitter/metrics/stats/user_metrics/jsonize_strong_links.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'wukong'
+
+class Mapper < Wukong::Streamer::RecordStreamer
+  def process *args, &blk
+    uid, sn, bag_of_links = args
+    yield [uid, jsonize(uid, sn, list)]
+  end
+
+  def jsonize uid, sn, bag_of_links
+    links        = pig_bag_to_nested_array(bag_of_links)
+    strong_links = links.inject([]){|a, sub_arr| a << {:user_id => sub_arr.first, :weight => sub_arr.last}} unless links.blank?
+    record = { :user_id => uid, :screen_name => sn, :strong_links => strong_links }.compact_blanks.to_json
+  end
+
+  def pig_bag_to_nested_array tuple
+    tuple.split("),(").map{|t| t.gsub(/[\(\{\}\)]/, '').split(",")} rescue nil # psh
+  end
+end
+
+Wukong::Script.new(Mapper, nil).run
