c7084e47a5bf1d74dd7d47fe3b60c051c6676067
Jacob <jacob.a.perkins@gmail.com>
2010-07-30 13:40:58 -0500

    script to bulk load and fix strong links

---
+++ social/network/twitter/apeyeye/throw_me_away/bulk_load_strong_links.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'json'
+require File.dirname(__FILE__)+'./bulk_load_streamer'
+
+#
+# to_i's on user ids
+# make sure I'm not my own best friend
+#
+
+class BulkLoadJsonAttribute < BulkLoadStreamer
+  def process user_id, json
+    return if json.blank? || user_id.blank?
+    db.insert(user_id.to_i, fix_json(json))
+    log.periodically{ print_progress }
+  end
+
+  def db
+    @db ||= TokyoDbConnection::TyrantDb.new(('tw_'+options.dataset).to_sym)
+  end
+
+  def fix_json json_string
+    hsh = JSON.parse(json_string)
+    hsh["user_id"] = hsh["user_id"].to_i
+    hsh["strong_links"].map!{|pair| pair = [pair["user_id"].to_i, pair["weight"].to_f]; pair[1] = 0.05 if pair.last == 0.0; pair }
+    hsh.to_json
+  end
+  
+  def print_progress
+    emit         log.progress(db.size)
+    $stderr.puts log.progress(db.size)
+  end
+end
+Wukong::Script.new(
+  BulkLoadJsonAttribute,
+  nil,
+  :map_speculative => "false",
+  :max_maps_per_node => 2
+  ).run
