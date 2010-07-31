a01da396d9703f66cb72d0b8842a9fd931a9b829
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-07-31 18:49:26 -0500

    user id mapping needs to specify the directory azkaban owns

--- social/network/twitter/base/parse/twitter_ids/user_id_mapping.rb
+++ social/network/twitter/base/parse/twitter_ids/user_id_mapping.rb
@@ -18,7 +18,7 @@ def extract_ids_from_a_follows_b unspliced, existing
   a_follows_b = File.join(unspliced, "a_follows_b")
   exists = system %Q{ hadoop fs -test -e #{a_follows_b} }
   a_follows_b = File.join(existing, "a_follows_b") unless exists
-  status = system %Q{pig -p REL=#{a_follows_b} -p RAW_REL_IDS_FILE=/tmp/all_seen_users/raw_rel_ids #{File.dirname(__FILE__)}/extract_ids_from_rel.pig}
+  status = system %Q{pig -p REL=#{a_follows_b} -p RAW_REL_IDS_FILE=/azk/tmp/all_seen_users/raw_rel_ids #{File.dirname(__FILE__)}/extract_ids_from_rel.pig}
   exit status unless status
 end
 
@@ -26,7 +26,7 @@ def extract_uid_sn_mapping unspliced, existing
   tweet = File.join(unspliced, "tweet")
   exists = system %Q{ hadoop fs -test -e #{tweet} }
   tweet = File.join(existing, "tweet") unless exists
-  status = system %Q{pig -p TWEET_FILE=#{tweet} -p RAW_ID_SNS_FILE=/tmp/all_seen_users/raw_id_sns #{File.dirname(__FILE__)}/extract_uid_sn_mapping.pig}
+  status = system %Q{pig -p TWEET_FILE=#{tweet} -p RAW_ID_SNS_FILE=/azk/tmp/all_seen_users/raw_id_sns #{File.dirname(__FILE__)}/extract_uid_sn_mapping.pig}
   exit status unless status
 end
 
@@ -39,7 +39,7 @@ def assemble_twitter_user_id unspliced, existing
   exists = system %Q{ hadoop fs -test -e #{twitter_user_search_id} }
   twitter_user_search_id = File.join(existing, "twitter_user_search_id") unless exists
 
-  status = system %Q{pig -p TW_UID=#{twitter_user_id} -p TW_SID=#{twitter_user_search_id} -p RAW_REL_IDS_FILE=/tmp/all_seen_users/raw_rel_ids -p RAW_ID_SNS_FILE=/tmp/all_seen_users/raw_id_sns -p MAPPING=/tmp/all_seen_users/all_user_info #{File.dirname(__FILE__)}/assemble_id_mapping.pig}
+  status = system %Q{pig -p TW_UID=#{twitter_user_id} -p TW_SID=#{twitter_user_search_id} -p RAW_REL_IDS_FILE=/azk/tmp/all_seen_users/raw_rel_ids -p RAW_ID_SNS_FILE=/azk/tmp/all_seen_users/raw_id_sns -p MAPPING=/azk/tmp/all_seen_users/all_user_info #{File.dirname(__FILE__)}/assemble_id_mapping.pig}
   exit status unless status
 end
 
