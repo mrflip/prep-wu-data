83b014fe006313ebc8a41a1865f79541578cff51
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-07-31 10:16:48 -0500

    minor changes to parse scripts

--- social/network/twitter/base/parse/twitter_ids/user_id_mapping.rb
+++ social/network/twitter/base/parse/twitter_ids/user_id_mapping.rb
@@ -15,7 +15,7 @@ existing  = ARGV[1]
 # Fails obviously if neither dir exists
 #
 def extract_ids_from_a_follows_b unspliced, existing
-  a_follows_b = File.join(unspliced, "a_follows_")
+  a_follows_b = File.join(unspliced, "a_follows_b")
   exists = system %Q{ hadoop fs -test -e #{a_follows_b} }
   a_follows_b = File.join(existing, "a_follows_b") unless exists
   status = system %Q{pig -p REL=#{a_follows_b} -p RAW_REL_IDS_FILE=/tmp/all_seen_users/raw_rel_ids #{File.dirname(__FILE__)}/extract_ids_from_rel.pig}
