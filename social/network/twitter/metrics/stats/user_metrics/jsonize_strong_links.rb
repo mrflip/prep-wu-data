b1b9712a8ff9e29fa3d50c1eb271601607569cc6
Jacob <jacob.a.perkins@gmail.com>
2010-08-02 13:18:20 -0500

    jsonizer for strong links should leave the array of links alone

--- social/network/twitter/metrics/stats/user_metrics/jsonize_strong_links.rb
+++ social/network/twitter/metrics/stats/user_metrics/jsonize_strong_links.rb
@@ -12,8 +12,10 @@ class Mapper < Wukong::Streamer::RecordStreamer
 
   def jsonize uid, sn, bag_of_links
     links        = pig_bag_to_nested_array(bag_of_links)
+    # can't be friends with myself, to_i everything
+    links.map{|link| link[0] = link[0].to_i; link[1] = link[1].to_f; link }.reject!{|x| x.first == uid.to_i}
     # strong_links = links.inject([]){|a, sub_arr| a << {:user_id => sub_arr.first, :weight => sub_arr.last}} unless links.blank?
-    record = { :user_id => uid, :screen_name => sn, :strong_links => links }.compact_blank.to_json
+    record = { :user_id => uid.to_i, :screen_name => sn, :strong_links => links }.compact_blank.to_json
   end
 
   def pig_bag_to_nested_array tuple
