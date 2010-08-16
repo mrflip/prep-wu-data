0b41d0b23d291381fe93937713c811f7e1c02823
Philip (flip) Kromer <flip@infochimps.org>
2010-08-16 12:50:25 -0500

    Newer mlb gameday stuff

--- sport/baseball/mlbgameday/snarf-mlbgameday-xml.sh
+++ sport/baseball/mlbgameday/snarf-mlbgameday-xml.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # must include trailing / on URL
 
 # for league in aaa afa asx ind milb mlb playertracker rok year_2006 aax afx bbc int min oly protected win year_2007 ; do
@@ -5,29 +7,31 @@
 # 	-Axml,txt --no-host-directories --cut-dirs=1 \
 # 	-nv -a /work/DataSources/Data_MLB/wget-`datename`.log \
 # 	--limit-rate=25k  \
-# 	http://gd2.mlb.com/components/game/${league}/
+# 	http://gdx.mlb.com/components/game/${league}/
 # done
 
-# http://gd2.mlb.com/components/game/mlb/year_2008/month_07/day_06/gid_2008_07_06_flomlb_colmlb_1/pbp/pitchers/150430.xml
+# http://gdx.mlb.com/components/game/mlb/year_2008/month_07/day_06/gid_2008_07_06_flomlb_colmlb_1/pbp/pitchers/150430.xml
 #                                    0   1         2        3       4                             5   6        7  
 
 #     oly aaa afa asx ind rok aax afx bbc int min win milb playertracker year_2006 year_2007 protected 
 logdir="/data/log/sport/baseball/mlb_gameday"
-ripdir="/data/ripd"
-gd_url="gd2.mlb.com/components/game"
+ripdir="/data/old_ripd"
+gd_url="gdx.mlb.com/components/game"
 mkdir -p $logdir
 cd $ripdir
 
 # # get the top-level directories
-# wget -r -l3 --no-clobber --relative --no-parent \
+# wget -r -l2 --no-clobber --relative --no-parent \
 #     -Axml,txt -nv -a $logdir/wget-`datename`.log \
-#     --random-wait -w0.5 \
-#     http://$gd_url/
-# | egrep -v 'year_200[567]' | egrep -v 'aaa/year_2008/month_0[123456]
-dirs=`find $gd_url -maxdepth 3 -mindepth 3 -type d | grep 'month_' `
-echo scraping `find $gd_url -maxdepth 3 -mindepth 3 -type d | grep 'month_' | cut -d/ -f4,5,6`
+#     --random-wait -w0.2 \
+#     http://$gd_url/mlb/
 
-for league_year_mo in $dirs ; do
+
+# dirs=`find $gd_url -maxdepth 3 -mindepth 3 -type d | grep 'month_' `
+dirs=$gd_url/mlb
+echo scraping $dirs
+
+for league_year_mo in $dirs/year_2008/month_{08,09,10,11,12}  $dirs/year_20{09,10}/month_* ; do
   wget -r -l9 --no-clobber --relative --no-parent \
     -Axml,txt -nv -a $logdir/wget-`datename`.log \
     --random-wait -w1.0  \
