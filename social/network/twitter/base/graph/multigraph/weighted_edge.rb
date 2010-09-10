fa43859e763ec59e636366708bc5175f55855412
Jacob <jacob.a.perkins@gmail.com>
2010-09-09 19:02:41 -0500

    weighted pagerank algorithm seems to work, needs slightly more testing before rerunning

--- social/network/twitter/base/graph/multigraph/weighted_edge.rb
+++ social/network/twitter/base/graph/multigraph/weighted_edge.rb
@@ -6,7 +6,8 @@ require 'wuclan/twitter/model' ; include Wuclan::Twitter::Model
 
 class EdgeWeightMapper < Wukong::Streamer::StructStreamer
   def process edge, *_
-    yield [edge.user_a_id, edge.user_b_id, edge.weight] if edge.weight > 0.0
+    # eg: 12345     940110     1     0     0.3
+    yield [edge.user_a_id, edge.user_b_id, edge.fo_sy, edge.at_sy, edge.weight] if edge.weight > 0.0
   end
 end
 
