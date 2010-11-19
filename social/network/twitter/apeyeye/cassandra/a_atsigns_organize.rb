2ab28375f84ed28cce4679d64a12b8de7258dc0b
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-11-19 12:58:52 -0600

    added organize script

---
+++ social/network/twitter/apeyeye/cassandra/a_atsigns_organize.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'wukong'
+require 'json'
+
+class Mapper < Wukong::Streamer::RecordStreamer
+  def process rsrc, user_a_id, user_b_id, rel_type, twid, crat, user_b_sn, in_reply_to_twid
+    yield [ [user_a_id, user_b_id].join(':'), twid, 
+      {:created_at=>crat, :user_b_sn=>user_b_sn.to_s, :rel_tw_id=>in_reply_to_twid}.compact.blank.to_json ]
+  end
+end
+
+Wukong::Script.new(Mapper).run
+
