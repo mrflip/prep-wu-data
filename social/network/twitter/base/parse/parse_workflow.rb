9adfc9be90cd3570ea7970569e503d9b76ea8793
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-02-07 19:48:16 +0000

    twitter parse workflow in progress

---
+++ social/network/twitter/base/parse/parse_workflow.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'swineherd' ; include Swineherd
+require 'swineherd/script/pig_script' ; include Swineherd::Script
+require 'swineherd/script/wukong_script'
+
+Settings.read('./parse_config.yaml')
+Settings.resolve!
+
+flow = Workflow.new(Settings['flow_id']) do
+  
+  api_parser    = WukongScript.new(File.join(Settings['wuclan_parse_scripts'], 'parse_twitter_api_requests-v2.rb'))
+  stream_parser = WukongScript.new(File.join(Settings['wuclan_parse_scripts'], 'parse_twitter_stream_requests-v2.rb'))
+  search_parser = WukongScript.new(File.join(Settings['wuclan_parse_scripts'], 'parse_twitter_search_request.rb'))
+  
+  task :parse_twitter_api do
+    api_parser.input << File.join(Settings['ripd_s3_url'], 'com.twitter', Settings['api_parse_regexp'])
+    api_parser.output << next_output(:parse_twitter_api)
+    api_parser.run
+  end
+
+  task :parse_twitter_search do
+    search_parser.input << File.join(Settings['ripd_s3_url'], 'com.twitter.search', Settings['search_parse_regexp'])
+    search_parser.output << next_output(:parse_twitter_search)
+    search_parser.run
+  end
+
+  task :parse_twitter_stream do
+    stream_parser.input << File.join(Settings['ripd_s3_url'], 'com.twitter.stream', Settings['stream_parse_regexp'])
+    stream_parser.output << next_output(:parse_twitter_stream)
+    stream_parser.run
+  end
+
+  task :parse_all => ["#{Settings['flow_id']}:parse_twitter_api", "#{Settings['flow_id']}:parse_twitter_search", "#{Settings['flow_id']}:parse_twitter_stream"] do
+  end
+
+end
+
+flow.workdir = Settings['hdfs_work_dir']
+flow.describe
+flow.run(Settings.rest.first)
+# flow.clean!
