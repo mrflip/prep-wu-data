49c1744d60031bacc321f61f0fe2e66915bf0f49
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-02-18 00:17:27 +0000

    fixed cedexis loader

--- web/analytics/cedexis/normalize_cedexis2.rb
+++ web/analytics/cedexis/normalize_cedexis2.rb
@@ -7,16 +7,16 @@ require 'json'
 class Mapper < Wukong::Streamer::RecordStreamer
 
   def recordize str
-    words = str.split(",")
-    words
+    fields = str.strip.split(",")
+    fields if fields.size == 13
   end
 
-  def process date, market_id, market_label, country_id, country_label, network_id, network_label, provider_id, provider_label, probe_id,probe_label, count, score, *_
-    row_key = [ provider_label, market_label, country_label, network_label ].map  { |entry| entry.strip.gsub(/\s/, '_').gsub(/\W/, '') }.join(":").downcase
+  def process date, market_id, market_label, country_id, country_label, network_id, network_label, provider_id, provider_label, probe_id, probe_label, count, score
+    row_key = [ provider_label, market_label, country_label, network_label ].map  { |entry| entry.strip.gsub(/\s/, '_').gsub(/[^\w\.]/, '') }.join(":").downcase
     col_fam = probe_label.gsub(/\s/, '_').downcase
-    col_nam = Time.parse(date).to_flat
-    col_val = { 'count' => count, 'score' => score }.to_json
-    yield [ row_key, col_fam, col_nam, col_val ]
+    date_long = Time.parse(date).to_flat
+    yield [ row_key, col_fam, 'count', count, date_long ]
+    yield [ row_key, col_fam, 'score', score, date_long ]
   end
 
 end
